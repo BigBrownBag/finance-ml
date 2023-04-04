f3abb340fd7110612299df01dbd0db418bdbb3b7
BigBrownBag <maraykin@gmail.com>
2023-04-04 22:48:11 +0500

    first commit

---
+++ client/src/scenes/predictions/index.tsx
@@ -0,0 +1,124 @@
+import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
+import { useGetKpisQuery } from "@/state/api";
+import { Box, Button, Typography, useTheme } from "@mui/material";
+import { useMemo, useState } from "react";
+import {
+  CartesianGrid,
+  Label,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import regression, { DataPoint } from "regression";
+
+const Predictions = () => {
+  const { palette } = useTheme();
+  const [isPredictions, setIsPredictions] = useState(false);
+  const { data: kpiData } = useGetKpisQuery();
+
+  const formattedData = useMemo(() => {
+    if (!kpiData) return [];
+    const monthData = kpiData[0].monthlyData;
+
+    const formatted: Array<DataPoint> = monthData.map(
+      ({ revenue }, i: number) => {
+        return [i, revenue];
+      }
+    );
+    const regressionLine = regression.linear(formatted);
+
+    return monthData.map(({ month, revenue }, i: number) => {
+      return {
+        name: month,
+        "Actual Revenue": revenue,
+        "Regression Line": regressionLine.points[i][1],
+        "Predicted Revenue": regressionLine.predict(i + 12)[1],
+      };
+    });
+  }, [kpiData]);
+
+  return (
+    <DashboardBox width="100%" height="100%" p="1rem" overflow="hidden">
+      <FlexBetween m="1rem 2.5rem" gap="1rem">
+        <Box>
+          <Typography variant="h3">Предсказание дохода</Typography>
+          <Typography variant="h6">
+            график доходов и прогнозируемый доход на основе простой линейной
+            регрессионной модели
+          </Typography>
+        </Box>
+        <Button
+          onClick={() => setIsPredictions(!isPredictions)}
+          sx={{
+            color: palette.grey[900],
+            backgroundColor: palette.grey[700],
+            boxShadow: "0.1rem 0.1rem 0.1rem 0.1rem rgba(0,0,0,.4)",
+          }}
+        >
+          Показать предсказываемый доход на следующий год
+        </Button>
+      </FlexBetween>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          data={formattedData}
+          margin={{
+            top: 20,
+            right: 75,
+            left: 20,
+            bottom: 80,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" stroke={palette.grey[800]} />
+          <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }}>
+            <Label value="Месяц" offset={-5} position="insideBottom" />
+          </XAxis>
+          <YAxis
+            domain={[12000, 26000]}
+            axisLine={{ strokeWidth: "0" }}
+            style={{ fontSize: "10px" }}
+            tickFormatter={(v) => `$${v}`}
+          >
+            <Label
+              value="Доход в USD"
+              angle={-90}
+              offset={-5}
+              position="insideLeft"
+            />
+          </YAxis>
+          <Tooltip />
+          <Legend verticalAlign="top" />
+          <Line
+            type="monotone"
+            dataKey="Actual Revenue"
+            name="Актуальный доход"
+            stroke={palette.primary.main}
+            strokeWidth={0}
+            dot={{ strokeWidth: 5 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="Regression Line"
+            name="Линия регрессии"
+            stroke="#8884d8"
+            dot={false}
+          />
+          {isPredictions && (
+            <Line
+              strokeDasharray="5 5"
+              dataKey="Predicted Revenue"
+              name="Предсказываемый доход"
+              stroke={palette.secondary[500]}
+            />
+          )}
+        </LineChart>
+      </ResponsiveContainer>
+    </DashboardBox>
+  );
+};
+
+export default Predictions;
